b0b47acbd77c04a1a0fe4cf6ef247c13d914f64b
rattapon <rattapon.s@ssd.co.th>
2013-08-13 12:09:53 +0700

    Added Initialize GameOfLife and Live up cells function with some test cases

--- GameOfLife.FSharp/GameOfLife.fs
+++ GameOfLife.FSharp/GameOfLife.fs
@@ -1,11 +1,20 @@
 ﻿namespace GameOfLife.FSharp
 
+open System
+    
     module GameOfLife = 
-
+    
         let initialize role column = 
-            let grid = Array2D.init role column (fun _ _ -> false) 
-            0
+            if role < 1 || column < 1 then raise (System.ArgumentException("Cannot initialize with size that is smaller than 1"))
+            let grid = Array2D.init role column (fun _ _ -> false)
+            grid
 
+        let wakeUpCell coordinates (grid:bool[,]) = 
+            let mutable newGrid :bool[,] = Array2D.copy grid
+            for coordinate in coordinates do
+                match coordinate with 
+                    | (x, y) -> newGrid.[x, y] <- true
+            newGrid         
 
 
 
@@ -14,3 +23,6 @@
 
         [<CompiledName("Initialize")>]
         let init role column = initialize role column
+
+        [<CompiledName("LiveCell")>]
+        let wake coordinates grid = wakeUpCell coordinates grid
--- GameOfLife.Test/GameOfLifeFSharpTest.cs
+++ GameOfLife.Test/GameOfLifeFSharpTest.cs
@@ -3,13 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.FSharp.Collections;
 using NUnit.Framework;
+using GameOfLife.FSharp;
 
 namespace GameOfLife.Test
 {
+
+    public static class Extensions
+    {
+        public static FSharpList<T> ToFSharpList<T>(this IEnumerable<T> source)
+        {
+            return ListModule.OfSeq(source);
+        }
+    }
+
     [TestFixture]
     class GameOfLifeFSharpTest
     {
+        [Test]
+        public void InitializeSuccessfully()
+        {
+            var cells = FSharp.GameOfLife.Initialize(10, 20);
+            Assert.AreEqual(200, cells.Length);
+        }
 
+        [TestCase(0, 10)]
+        [TestCase(0, 0)]
+        [TestCase(10, 0)]
+        [TestCase(-10, -10)]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldThrowExceptionWhenInitializeWithNumberSmallerThan1(int x , int y)
+        {
+            var cells = FSharp.GameOfLife.Initialize(x, y);
+            Assert.Fail();
+        }
+
+        [Test]
+        public void LiveUpCellShouldBeSuccess()
+        {
+            var cells = FSharp.GameOfLife.Initialize(3, 3);
+            var liveCells = new List<Tuple<int, int>>()
+            {
+                Tuple.Create(0, 0),
+                Tuple.Create(0, 1),
+                Tuple.Create(0, 2),
+            };
+           
+            var newCells = FSharp.GameOfLife.LiveCell(liveCells.ToFSharpList(), cells);
+
+            Assert.AreEqual(true, newCells[0, 0]);
+            Assert.AreEqual(true, newCells[0, 1]);
+            Assert.AreEqual(true, newCells[0, 2]);
+            
+        }
     }
 }
