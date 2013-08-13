c130754e0cc7a8dd9768ddcab5da962323d27f09
rattapon <rattapon.s@ssd.co.th>
2013-08-13 10:00:00 +0700

    Created F# projects

---
+++ GameOfLife.FSharp/GameOfLife.fs
@@ -0,0 +1,16 @@
+﻿namespace GameOfLife.FSharp
+
+    module GameOfLife = 
+
+        let initialize role column = 
+            let grid = Array2D.init role column (fun _ _ -> false) 
+            0
+
+
+
+
+
+
+
+        [<CompiledName("Initialize")>]
+        let init role column = initialize role column
